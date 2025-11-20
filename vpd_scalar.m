bc749c5425e3981dff93e291bbe8042b6ca4189d
WangYong <keyide9264@gmail.com>
2025-11-20 09:54:31 +0800

    Update vpd_scalar.m

--- vpd_scalar.m
+++ vpd_scalar.m
@@ -1,13 +1,9 @@
 function [Fvpd]=vpd_scalar(data,para)
-
 VPD=data(:,4);   %VPD
-
 VPDmax=para(7);        % VPDmax for temperature scalar
 VPDmin=para(8);       % VPDmin for temperature scalar
 
 Fvpd=(VPDmax-VPD)/(VPDmax-VPDmin);
 Fvpd(VPD>=VPDmax)=0;
 Fvpd(VPD<=VPDmin)=1;
-
 end
-
