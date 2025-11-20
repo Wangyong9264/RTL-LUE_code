ae725c2848c9b1fa686cc21f6313c40db4aab534
WangYong <keyide9264@gmail.com>
2025-11-20 09:53:40 +0800

    Update radiation_scalar.m

--- radiation_scalar.m
+++ radiation_scalar.m
@@ -1,12 +1,9 @@
 function [fPPFDsu,fPPFDsh]=radiation_scalar(PARsu, PARsh, para)
-
 fPPFD_a=para(2);  % parameter a for PPFD scalar
 fPPFD_b=1;        % parameter b for PPFD scalar, 1 mol m-2 hh-1
 
 PPFDsu=(4.55.*PARsu/0.5)/(2);
 PPFDsh=(4.55.*PARsh/0.5)/(2);
-
 fPPFDsu=fPPFD_b./(fPPFD_a*PPFDsu+fPPFD_b);
 fPPFDsh=fPPFD_b./(fPPFD_a*PPFDsh+fPPFD_b);
-
 end
