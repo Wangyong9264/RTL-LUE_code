26bd561ff83249635ef41378e46e67766fd5c14b
WangYong <keyide9264@gmail.com>
2025-11-20 09:54:11 +0800

    Update two_leaf_separate.m

--- two_leaf_separate.m
+++ two_leaf_separate.m
@@ -1,9 +1,8 @@
 function [LAIsu, LAIsh]=two_leaf_separate(cosza, para, data)
-
 omiga=para(3);    % clumping index
 LAI=data(:,2);   % filtered LAI
 
 LAIsu=2*cosza.*(1-exp(-0.5*omiga*(LAI./cosza)));
 LAIsh=LAI-LAIsu;
-
 end
+
