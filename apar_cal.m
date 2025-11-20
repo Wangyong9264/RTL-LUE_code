5ba556b84ec2b553df15ef24cc41ff9d65a6af0a
WangYong <keyide9264@gmail.com>
2025-11-20 09:52:14 +0800

    Update apar_cal.m

--- apar_cal.m
+++ apar_cal.m
@@ -1,13 +1,8 @@
 function [APARsu, APARsh]=apar_cal(cosza, para, data, PARsh, PARsu)
-
 abledo=para(4);        % albedo
-
 [LAIsu, LAIsh]=two_leaf_separate(cosza, para, data);    
-
 APARsh=(1-abledo)*PARsh.*LAIsh;
 APARsu=(1-abledo)*PARsu.*LAIsu;
-
 APARsu(isnan(APARsu))=0;
 APARsh(isnan(APARsh))=0;
-
 end
